538846a6ef1ed583f8e82ebe5bedd9736af9367a
Eggy115 <79756011+Eggy115@users.noreply.github.com>
2023-04-02 18:11:02 +0100

    Create asynchronous tcp echo server.rs

---
+++ asynchronous tcp echo server.rs
@@ -0,0 +1,20 @@
+use tokio::net::{TcpListener, TcpStream};
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let listener = TcpListener::bind("127.0.0.1:8080").await?;
+    loop {
+        let (mut socket, _) = listener.accept().await?;
+        tokio::spawn(async move {
+            let mut buf = [0; 1024];
+            loop {
+                let n = socket.read(&mut buf).await.unwrap();
+                if n == 0 {
+                    return;
+                }
+                socket.write_all(&buf[0..n]).await.unwrap();
+            }
+        });
+    }
+}
